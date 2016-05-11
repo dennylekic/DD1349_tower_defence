fd54401eae542e8c930574a903d20ae12abda047
oscaraberg <oscarabergg@gmail.com>
2016-05-11 12:19:19 +0200

    merge class game and gamepanel to one class game

--- code/src/Game.java
+++ code/src/Game.java
@@ -2,14 +2,19 @@ import javax.swing.JFrame;
 
 public class Game {
 
+	private static final int WIDTH = 320, HEIGHT = 240, SCALE  = 2;
+	
 	public static void main(String[] args) {
+		new Game();
 		
-		JFrame window = new JFrame("Tower Defence");
-		window.setContentPane(new GamePanel());
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.pack();
-		window.setVisible(true);
 	}
-
+	
+	public Game() {
+		JFrame window = new JFrame("tower defence");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(WIDTH * SCALE, HEIGHT * SCALE);
+		window.add(new Tile(10, 10));
+		window.setVisible(true);
+		
+	}
 }
