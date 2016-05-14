02ac44b906a4b3eade26bc08e246f4f373375c3d
oscaraberg <oscarabergg@gmail.com>
2016-05-14 17:00:03 +0200

    add enemy class

---
+++ code/src/Enemy.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Enemy extends Object{
+
+	public Enemy(int x, int y, Color c) {
+		setX(x);
+		setY(y);
+		setColor(c);
+		setObjektSize(30);
+	}
+	
+	public void Draw(Graphics g) {
+		g.setColor(c);
+		g.fillOval(x, y, objektSize, objektSize);
+	}
+	
+	public void Update() {
+		
+	}
+	
+}
